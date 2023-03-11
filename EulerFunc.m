41bd226eb2317d3c3cc0aa648b18dd6a67b4366a
sara42lee <127612502+sara42lee@users.noreply.github.com>
2023-03-11 18:51:48 +0330

    Add files via upload

---
+++ EulerFunc.m
@@ -0,0 +1,15 @@
+function [X,Y]= EulerFunc(f,a,b,ya,n) % which f is a function handle
+h = (b-a)/n ; % steps
+X = a:h:b ;
+Y(1) = ya ;
+for i = 1:n
+    Y(i+1) = Y(i) + h*f(X(i),Y(i)) ;
+end
+%X = X(:) ;
+%Y = Y(:) ;
+T = table(X,Y) ;
+writetable(T,"coordinates.xlsx") ;
+plot(X,Y)
+X ;
+Y ;
+end
---
+++ EulerMethod.m
@@ -0,0 +1,10 @@
+% initializing
+n = 100 ;
+a = 0 ;
+b = 1 ;
+ya = 0 ;
+
+f = @(x,y) x.^2 + y ; % or any other functions
+[X,Y] = EulerFunc(f,a,b,ya,n) ;
+
+
